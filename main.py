a9d48f5b334b55ac2636c10b44e5833ddd3defbd
Petr Novotný <krkabol@gmail.com>
2025-05-12 14:19:35 +0200

    proceeded all cols

--- main.py
+++ main.py
@@ -15,7 +15,14 @@ from export_pipeline.columns.col22_Habitat import Column22_Habitat
 from export_pipeline.columns.col23_Habitus import Column23_Habitus
 from export_pipeline.columns.col24_Bemerkungen import Column24_Bemerkungen
 from export_pipeline.columns.col2_HerbNummer import Column2_HerbNummer
+from export_pipeline.columns.col33_exactness import Column33_exactness
+from export_pipeline.columns.col34_quadrant import Column34_quadrant
+from export_pipeline.columns.col35_quadrant_sub import Column35_quadrnat_sub
+from export_pipeline.columns.col36_col37_altitude import Columns36_37_altitude
+from export_pipeline.columns.col38_digital_image import Column38_digital_image
+from export_pipeline.columns.col39_digital_image_obs import Column39_digital_image_obs
 from export_pipeline.columns.col3_collectionID import Column3_CollectionID
+from export_pipeline.columns.col40_observation import Column40_observation
 from export_pipeline.columns.col4_Collection import Column4_Collection
 from export_pipeline.columns.col50_helper import Column50_helper
 from export_pipeline.columns.col5_status import Column5_status
@@ -24,6 +31,7 @@ from export_pipeline.columns.col7_Sammler import Column7_Sammler
 from export_pipeline.columns.col8_series import Column8_series
 from export_pipeline.columns.col9_series_number import Column9_series_number
 from export_pipeline.columns.col11_alt_number import Column11_alt_number
+from export_pipeline.columns.col25_col32_coords import Columns25_32_coords
 from export_pipeline.pipeline import Pipeline
 from export_pipeline.columns.col1_id import Column1_ID
 
@@ -56,7 +64,15 @@ def main():
         Column21_Fundort_engl(),
         Column22_Habitat(),
         Column23_Habitus(),
-        Column24_Bemerkungen()
+        Column24_Bemerkungen(),
+        Columns25_32_coords(),
+        Column33_exactness(),
+        Column34_quadrant(),
+        Column35_quadrnat_sub(),
+        Columns36_37_altitude(),
+        Column38_digital_image(),
+        Column39_digital_image_obs(),
+        Column40_observation()
     ]
 
     pipeline = Pipeline(steps, MDB_PATH)
