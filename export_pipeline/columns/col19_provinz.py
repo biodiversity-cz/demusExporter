2597c426e4f51cffe518fadb1e949320a3f13d79
Petr Novotný <krkabol@gmail.com>
2025-05-23 13:03:23 +0200

    use OPkres for provinz

--- export_pipeline/columns/col19_provinz.py
+++ export_pipeline/columns/col19_provinz.py
@@ -1,5 +1,15 @@
 from .base import BaseStep
+import pandas
 
 class Column19_provinz(BaseStep):
 
     _column_name="provinz"
+
+    def compute(self) -> pandas.DataFrame:
+        self._sbirky['Okres_SX'] = self._sbirky['Okres_SX'].fillna('')
+        result = self._sbirky.apply(
+            lambda row: f"{row.get('Okres_SX', '')}".strip(),
+            axis=1
+        )
+
+        return pandas.DataFrame({self._column_name: result})
