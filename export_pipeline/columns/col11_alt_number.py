b20430ed768a3eb4b2dfb69029b0f5b8bf37ecce
Petr Novotný <krkabol@gmail.com>
2025-05-26 16:13:50 +0200

    proper alt nr handling

--- export_pipeline/columns/col11_alt_number.py
+++ export_pipeline/columns/col11_alt_number.py
@@ -7,9 +7,14 @@ class Column11_alt_number(BaseStep):
     def compute(self) -> pandas.DataFrame:
         self._sbirky['JinaC_S'] = self._sbirky['JinaC_S'].fillna('s.n.')
 
-        result = self._sbirky.apply(
-            lambda row: str(row.get('JinaC_S', '')).strip(),
-            axis=1
-        )
-
+        result = self._sbirky['JinaC_S'].map(self.extract_non_numeric_or_sn)
         return pandas.DataFrame({self._column_name: result})
+
+    def extract_non_numeric_or_sn(self, value) -> str:
+        value_str = str(value).strip()
+        if value_str == '':
+            return 's.n.'
+        elif value_str.isdigit():
+            return ''
+        else:
+            return value_str
