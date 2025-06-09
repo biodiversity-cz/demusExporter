fee8a966f8acc497611b0f760eb7161db51afb4f
Petr Novotný <krkabol@gmail.com>
2025-06-09 13:49:13 +0200

    use 7 digits for jacq

--- export_jacq/columns/col2_HerbNummer.py
+++ export_jacq/columns/col2_HerbNummer.py
@@ -5,17 +5,17 @@ class Column2_HerbNummer(BaseStep):
     _column_name="HerbNummer"
 
     def _convert_row(self, row) -> str:
-        por_c = row.get('PorC_S', '')
+        por_c = str(row.get('PorC_S', ''))
+        if len(por_c) < 7:
+            por_c = por_c.zfill(7)
         lomeni = row.get('Lomeni_S', '')
         if lomeni != '_':
             result = f"{por_c}/{lomeni}"
         else:
             result = por_c
 
-        result_str = str(result)
-        if len(result_str) < 7:
-            result_str = result_str.zfill(7)
-        return result_str
+        return result
+
 
     def compute(self) -> pandas.DataFrame:
         result = self._sbirky.apply(
